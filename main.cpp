31f48590c8cc5bd561593969ffb97dd644cc933c
catalin.voineag <catalin.voineag@onthebeach.co.uk>
2021-03-17 22:34:34 +0000

    Added Assets, score and timebar
    This PR adds:
    - Background for the game
    - Three moving clouds
    - Moving bee
    - Pause function triggered by pressing the Return key
    - Shrinking timebar
    - Hardcoded text like `Score`
    
    Change considerations:
    - Moved the build under the Debug folder, removed the build folder. This
      seems to be the convention

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <sstream>
 #include <SFML/Audio.hpp>
 #include <SFML/Graphics.hpp>
 #include <SFML/Window.hpp>
@@ -7,18 +8,96 @@
 
 using namespace sf;
 
-
 int main() {
 
-	sf::RenderWindow window(sf::VideoMode(800, 600), "SFML window");
+	sf::RenderWindow window(sf::VideoMode(1920, 1080), "SFML window");
 	window.setFramerateLimit(60);
 
 	sf::Texture textureBackground;
-	textureBackground.loadFromFile("../graphics/background.png");
+	textureBackground.loadFromFile("graphics/background.png");
 
 	sf::Sprite spriteBackground;
 	spriteBackground.setTexture(textureBackground);
 	spriteBackground.setPosition(0, 0);
+
+	Texture textureTree;
+	textureTree.loadFromFile("graphics/tree.png");
+	Sprite spriteTree;
+	spriteTree.setTexture(textureTree);
+	spriteTree.setPosition(810, 0);
+
+	Texture textureBee;
+	textureBee.loadFromFile("graphics/bee.png");
+	Sprite spriteBee;
+	spriteBee.setTexture(textureBee);
+	spriteBee.setPosition(0, 800);
+	bool beeActive = false;
+	float beeSpeed = 0.0f;
+
+	Texture textureCloud;
+	textureCloud.loadFromFile("graphics/cloud.png");
+	Sprite spriteCloud1;
+	Sprite spriteCloud2;
+	Sprite spriteCloud3;
+
+	spriteCloud1.setTexture(textureCloud);
+	spriteCloud2.setTexture(textureCloud);
+	spriteCloud3.setTexture(textureCloud);
+
+	spriteCloud1.setPosition(0, 0);
+	spriteCloud2.setPosition(0, 250);
+	spriteCloud3.setPosition(0, 500);
+
+	bool cloud1Active = false;
+	bool cloud2Active = false;
+	bool cloud3Active = false;
+
+	float cloud1Speed = 0.0f;
+	float cloud2Speed = 0.0f;
+	float cloud3Speed = 0.0f;
+
+	int score = 0;
+	sf::Text messageText;
+	sf::Text scoreText;
+	// We need to choose a font
+	Font font;
+	font.loadFromFile("fonts/KOMIKAP_.ttf");
+	// Set the font to our message
+	messageText.setFont(font);
+	scoreText.setFont(font);
+	// Assign actual message
+	messageText.setString("Pres Enter to start!");
+	scoreText.setString("Score = 0");
+	// Make it really big	
+	messageText.setCharacterSize(75);
+	scoreText.setCharacterSize(100);
+	// Choose a color
+	messageText.setFillColor(Color::White);
+	scoreText.setFillColor(Color::White);
+	// Position the text
+	FloatRect textRect = messageText.getLocalBounds();
+	messageText.setOrigin(textRect.left +
+		textRect.width / 2.0f,
+		textRect.top +
+		textRect.height / 2.0f
+	);
+	messageText.setPosition(1920 / 2.0f, 1080 / 2.0f);
+	scoreText.setPosition(20, 20);
+
+	Clock clock;
+	// Time Bar
+	RectangleShape timeBar;
+	float timeBarStartWidth = 400;
+	float timeBarHeight = 80;
+	timeBar.setSize(Vector2f(timeBarStartWidth, timeBarHeight));
+	timeBar.setFillColor(Color::Red);
+	timeBar.setPosition((1920 / 2) - timeBarStartWidth / 2, 980);
+
+	Time gameTimeTotal;
+	float timeRemaining = 6.0f;
+	float timeBarWidthPerSecond = timeBarStartWidth / timeRemaining;
+
+	bool paused = true;
 
 	while (window.isOpen())
 	{
@@ -28,8 +107,143 @@ int main() {
 		   if (event.type == sf::Event::Closed)
 			   window.close();
 	   }
+
+	   if (Keyboard::isKeyPressed(Keyboard::Return)) {
+		   paused = false;
+
+		   score = 0;
+		   timeRemaining = 5;
+	   }
+
+	   if (!paused) {
+		   Time deltaTime = clock.restart();
+
+		   // Substract from the time remaining
+		   timeRemaining -= deltaTime.asSeconds();
+		   // update the timebar size
+		   timeBar.setSize(Vector2f(
+				   timeBarWidthPerSecond * timeRemaining,
+				   timeBarHeight
+			   )
+		   );
+
+		   if (timeRemaining <= 0.0f) {
+			   paused = true;
+
+			   messageText.setString("Out of time!");
+
+			   //Reposition the text based on its new size
+			   FloatRect textRect = messageText.getLocalBounds();
+			   messageText.setOrigin(
+				   (textRect.left + textRect.width) / 2.0f,
+				   (textRect.top + textRect.height) / 2.0f
+			   );
+			   messageText.setPosition(1920 / 2.0f, 1080 /2.0f);
+		   }
+
+		   if (!beeActive) {
+				
+			   // Seed generator
+			   srand((int)time(0) * 10);
+			   beeSpeed = (rand() % 200) + 200;
+
+			   srand((int)time(0) * 10);
+			   float beeHeight = (rand() % 500) + 500;
+			   spriteBee.setPosition(2000, beeHeight);
+			   beeActive = true;
+		   } else {
+			   spriteBee.setPosition(
+				   spriteBee.getPosition().x -
+				   (beeSpeed * deltaTime.asSeconds()),
+				   spriteBee.getPosition().y
+			   );
+
+			   if (spriteBee.getPosition().x < -100) {
+				   beeActive = false;
+			   }
+		   }
+
+		   if (!cloud1Active) {
+			   srand((int)time(0) * 10);
+			   cloud1Speed = (rand() % 200);
+
+			   float cloudHeight = (rand() % 150);
+			   spriteCloud1.setPosition(-200, cloudHeight);
+			   cloud1Active = true;
+		   } else {
+			   spriteCloud1.setPosition(
+					spriteCloud1.getPosition().x +
+					(cloud1Speed * deltaTime.asSeconds()),
+					spriteCloud1.getPosition().y
+				);
+
+			   if (spriteCloud1.getPosition().x > 1920) {
+				   cloud1Active = false;
+			   }
+		   }
+
+		   if (!cloud2Active) {
+			   srand((int)time(0) * 20);
+			   cloud2Speed = (rand() % 200);
+
+			   float cloudHeight = (rand() % 300) - 150;
+			   spriteCloud2.setPosition(-200, cloudHeight);
+			   cloud2Active = true;
+		   } else {
+			   spriteCloud2.setPosition(
+					spriteCloud2.getPosition().x +
+					(cloud2Speed * deltaTime.asSeconds()),
+					spriteCloud2.getPosition().y
+				);
+
+			   if (spriteCloud2.getPosition().x > 1920) {
+				   cloud2Active = false;
+			   }
+		   }
+
+		   if (!cloud3Active) {
+			   srand((int)time(0) * 30);
+			   cloud3Speed = (rand() % 200);
+
+			   float cloudHeight = (rand() % 450) - 150;
+			   spriteCloud3.setPosition(-200, cloudHeight);
+			   cloud3Active = true;
+		   } else {
+			   spriteCloud3.setPosition(
+					spriteCloud3.getPosition().x +
+					(cloud3Speed * deltaTime.asSeconds()),
+					spriteCloud3.getPosition().y
+				);
+
+			   if (spriteCloud3.getPosition().x > 1920) {
+				   cloud3Active = false;
+			   }
+		   }
+
+		   // Updat the score text
+		   std::stringstream ss;
+		   ss << "Score = " << score;
+		   scoreText.setString(ss.str());
+	   }
+
 	   window.clear();
+
 	   window.draw(spriteBackground);
+
+	   window.draw(spriteCloud1);
+	   window.draw(spriteCloud2);
+	   window.draw(spriteCloud3);
+
+	   window.draw(spriteTree);
+	   window.draw(spriteBee);
+
+	   window.draw(scoreText);
+	   window.draw(timeBar);
+
+	   if (paused) {
+		   window.draw(messageText);
+	   }
+
 	   window.display();
 	}
 
